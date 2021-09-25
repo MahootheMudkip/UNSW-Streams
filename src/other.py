0d18375288b798a31f014b74a5a7a7603c13fd48
Michael <michael.gao@student.unsw.edu.au>
2021-09-25 15:28:48 +1000

    Implemented clear_v1, clearing users and channels dicts

--- src/other.py
+++ src/other.py
@@ -2,5 +2,6 @@ from src.data_store import data_store
 
 def clear_v1():
     store = data_store.get()
-    store['users'] = []
+    store["users"] = {}
+    store["channels"] = {}
     data_store.set(store)
