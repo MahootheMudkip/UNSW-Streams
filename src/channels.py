1847f12ca0592dfd1b9e25a2f10db94d2d2b01bf
Jay <z5341020@ad.unsw.edu.au>
2021-10-03 05:45:59 +1100

    style fix

--- src/channels.py
+++ src/channels.py
@@ -1,87 +1,88 @@
 from src.data_store import data_store
 from src.error import InputError, AccessError
- 
+
 """
-Provies a list of public channels a user is part of.
+Provides a list of all channels a user is part of.
 Parameters:
-   auth_user_id (int): The user id of the person whose public chhanels are to displayed
- 
+   auth_user_id (int): The user id of the person whose public channels are to be displayed
+
 Exceptions:
    AccessError:
        - When auth_user_id is invalid
- 
+
 Return Type:
-   A dictionay of list of dictionaires
+   A list of dictionaries each of which contains the channel_id and name of that channel
 """
 def channels_list_v1(auth_user_id):
-  
-   store = data_store.get()
-   users = store["users"]
-   channels = store["channels"]
- 
-   # Checks if auth_user_id is invalid.
-   if auth_user_id not in users.keys():
-       raise AccessError("Invalid user")
- 
-   return_channels = {"channels" : []}
- 
-    for each_channel in channels:                                                   #go through each channel's dict
-        for all_members_each_channel in channels[each_channel]["all_members"]:    #go through all members of every channel dict
-            if all_members_each_channel == auth_user_id:                            #if auth user id present
+    # get data from data_store
+    store = data_store.get()
+    users = store["users"]
+    channels = store["channels"]
+
+    # Checks if auth_user_id is invalid.
+    if auth_user_id not in users.keys():
+        raise AccessError("Invalid user")
+
+    # List of channels to be returned
+    return_list = []
+
+    # go through each channel's dict
+    for channel_id, channel in channels.items():
+        # go through all members of every channel dict
+        for member_id in channel["all_members"]:
+            #if auth user id present in current channel
+            if member_id == auth_user_id:
                 channel_details = {
-                    "channel_id" : each_channel,
-                    "name" : channels[each_channel]["channel_name"]
+                    "channel_id" : channel_id,
+                    "name" : channel["channel_name"]
                 }
-                return_channels["channels"].append(channel_details)
-                  
-   return return_channels
- 
-######################################################################
- 
+                return_list.append(channel_details)
+
+    return {
+        "channels" : return_list
+    }
+
 def channels_listall_v1(auth_user_id):
-   return {
-       'channels': [
-           {
-               'channel_id': 1,
-               'name': 'My Channel',
-           }
-       ],
-   }
-####################################################################
+    return {
+        'channels': [
+        	{
+        		'channel_id': 1,
+        		'name': 'My Channel',
+        	}
+        ],
+    }
+
 def channels_create_v1(auth_user_id, name, is_public):
- 
-   # Get user and channel data
-   store = data_store.get()
-   users = store["users"]
-   channels = store["channels"]
- 
-   # check whether auth_user_id exists
-   if auth_user_id not in users.keys():
-       raise AccessError("Invalid auth_user_id")
- 
-   # check length of channel name is valid
-   if not 1 <= len(name) <= 20:
-       raise InputError("Invalid channel name")
- 
-   # Create new channel and initialise fields
-   new_channel = {
-       "channel_name": name,
-       "is_public": is_public,
-       "owner_members": [auth_user_id],
-       "all_members": [auth_user_id],
-   }
- 
-   # Generate new channel_id
-   channel_id = len(channels)
- 
-   # Store changes back into database
-   channels[channel_id] = new_channel
-   store["channels"] = channels
-   data_store.set(store)
- 
-   return {
-       'channel_id': channel_id,
-   }
- 
 
+    # Get user and channel data
+    store = data_store.get()
+    users = store["users"]
+    channels = store["channels"]
 
+    # check whether auth_user_id exists
+    if auth_user_id not in users.keys():
+        raise AccessError("Invalid auth_user_id")
+
+    # check length of channel name is valid
+    if not 1 <= len(name) <= 20:
+        raise InputError("Invalid channel name")
+
+    # Create new channel and initialise fields
+    new_channel = {
+        "channel_name": name,
+        "is_public": is_public,
+        "owner_members": [auth_user_id],
+        "all_members": [auth_user_id],
+    }
+
+    # Generate new channel_id
+    channel_id = len(channels)
+
+    # Store changes back into database
+    channels[channel_id] = new_channel
+    store["channels"] = channels
+    data_store.set(store)
+
+    return {
+        'channel_id': channel_id,
+    }
