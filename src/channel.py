db48616daead99deac6d5ec62a37780d2845f343
Juan Carlos Puyo <j.puyo@student.unsw.edu.au>
2021-10-20 10:52:18 +1100

    Fixed pylint

--- src/channel.py
+++ src/channel.py
@@ -277,7 +277,6 @@ def channel_leave_v1(token, channel_id):
     '''
     auth_user_id = get_auth_user_id(token)
     store = data_store.get()
-    users = store["users"]
     channels = store["channels"]
     
     # checks for invalid channel_id.
