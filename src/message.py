643ecee05a29004c3860a8206c1bedb2bc5d3b3a
Maximilian Huh <z5363771@ad.unsw.edu.au>
2021-10-19 18:05:42 +1100

    implemented message/edit/v1 and uncommented out route in server.py

--- src/message.py
+++ src/message.py
@@ -52,4 +52,66 @@ def message_send_v1(token, channel_id, message):
         "message_id": message_id_tracker
     }
 
+def message_send_v1(token, message_id, message):
+    # get auth_user_id from token (this function handles all exceptions)
+    auth_user_id = get_auth_user_id(token)
+
+    store = data_store.get()
+    channels = store["channels"]
+    dms = store["dms"]
+
+    location_found = False
+    location_info = {}
+    location_type = ""
+    # check message_id is within a channel/dm that the user has joined
+
+    # find location of message_id
+    for channel in channels:
+        if auth_user_id in channel["all_members"]:
+            if message_id in channel["messages"]:
+                location_found = True
+                location_info = channel
+                location_type = "channel"
+                break
     
+    if location_found == False:
+        for dm in dms:
+            if auth_user_id in dm["members"]:
+                if message_id in dm["messages"]:
+                    location_found = True
+                    location_info = dm
+                    location_type = "dm"
+                    break
+    
+    if location_found == False:
+        raise InputError("message_id not within a channel/dm that the user has joined")
+
+    # if it reaches this point, a valid location has been found
+    user_info = store["users"][auth_user_id]
+    user_is_owner = user_info["is_owner"]
+    
+    message_info = store["messages"][message_id]
+    message_u_id = message_info["u_id"]
+    if auth_user_id != message_u_id:
+        if location_type == "channel":
+            if auth_user_id not in location_info["owner_members"] and user_is_owner == False:
+                # not a owner or global owner
+                raise AccessError("Unauthorised user")
+        elif location_type == "dm":
+            if auth_user_id != location_info["owner"] and user_is_owner == False:
+                raise AccessError("Unauthorised user")
+    
+    # if it reaches this point, user has proper authorisation
+    if len(message) > 1000:
+        raise InputError("Message is too long")    
+    
+    # if message is empty, delete it 
+    if len(message) == 0:
+        location_info["messages"].remove(message_id)
+        # this will work whether location_type is a channel or dm
+        store["messages"].pop(message_id)
+    else:
+        message_info["message"] = message
+
+    data_store.set(store)
+    return {}
