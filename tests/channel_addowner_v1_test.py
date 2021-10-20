37092fbd67370f76a9c8369763c7c0a94f4bde03
Your Name <z5375997@ad.unsw.edu.au>
2021-10-21 03:50:03 +1100

    Finished tests for channel/addowner/v1

--- tests/channel_addowner_v1_test.py
+++ tests/channel_addowner_v1_test.py
@@ -46,6 +46,15 @@ def initial_data():
     })
     public_channel = json.loads(public_channel_response.text)
     public_channel_id = public_channel["channel_id"]
+    # Make users join the public channel
+    requests.post(config.url + 'channel/join/v2', json={
+        "token": user2_token,
+        "channel_id": public_channel_id
+    })
+    requests.post(config.url + 'channel/join/v2', json={
+        "token": user3_token,
+        "channel_id": public_channel_id
+    })
     # Make private channel
     private_channel_response = requests.post(config.url + 'channels/create/v2', json={
         "token": user1_token,
@@ -54,14 +63,31 @@ def initial_data():
     })
     private_channel = json.loads(private_channel_response.text)
     private_channel_id = private_channel["channel_id"]
+    # Invite users to private channel
+    requests.post(config.url + 'channel/invite/v2', json={
+        "token": user1_token,
+        "channel_id": private_channel_id,
+        "u_id": user2_id
+    })
+    requests.post(config.url + 'channel/invite/v2', json={
+        "token": user1_token,
+        "channel_id": private_channel_id,
+        "u_id": user3_id
+    })
     # Make channel created by user2
     channel0_response = requests.post(config.url + 'channels/create/v2', json={
-        "token": user1_token,
-        "name": "Rainbow Six Siege",
+        "token": user2_token,
+        "name": "channel0",
         "is_public": True
     })
     channel0 = json.loads(channel0_response.text)
     channel0_id = channel0["channel_id"]
+    # Join channel0 with global user
+    requests.post(config.url + 'channel/invite/v2', json={
+        "token": user1_token,
+        "channel_id": channel0_id,
+        "u_id": user2_id
+    })
     
     values = {
         "user1_token": user1_token,
@@ -139,21 +165,68 @@ def test_auth_user_isnt_owner():
     user2_id = initial_data["user2_id"]
     user3_token = initial_data["user3_token"]
     user3_id = initial_data["user3_id"]
+    public_channel_id = initial_data["public_channel_id"]
 
-    resp = requests.post(config.url + 'channel/addowner/v1', json={"token": user2_token, "channel_id": 5444, "u_id": user3_id})
+    resp = requests.post(config.url + 'channel/addowner/v1', json={"token": user2_token, "channel_id": public_channel_id, "u_id": user3_id})
     assert(resp.status_code == ACCESS_ERROR)
-    resp2 = requests.post(config.url + 'channel/addowner/v1', json={"token": user3_token, "channel_id": 243, "u_id": user2_id})
+    resp2 = requests.post(config.url + 'channel/addowner/v1', json={"token": user3_token, "channel_id": public_channel_id, "u_id": user2_id})
     assert(resp2.status_code == ACCESS_ERROR)
 
 # u_id refers to a user who is already an owner of the channel
 def test_u_id_is_an_owner(initial_data):
-    user1_id = initial_data["user1_id"]
-    user2_token = initial_data["user2_token"]
+    user2_id = initial_data["user2_id"]
+    user1_token = initial_data["user1_token"]
     channel0_id = initial_data["channel0_id"]
     
-    resp = requests.post(config.url + 'channel/addowner/v1', json={"token": user2_token, "channel_id": 5444, "u_id": user1_id})
-    assert(resp.status_code == ACCESS_ERROR)
+    resp = requests.post(config.url + 'channel/addowner/v1', json={"token": user1_token, "channel_id": channel0_id, "u_id": user2_id})
+    assert(resp.status_code == INPUT_ERROR)
 
 
+# test adds two owners to the private channel
 def test_adding_owner_private_channel(initial_data):
-    return {}
+    user1_token = initial_data["user1_token"]
+    user2_id = initial_data["user2_id"]
+    user2_token = initial_data["user2_token"]
+    user3_id = initial_data["user3_id"]
+    private_channel_id = initial_data["private_channel_id"]
+
+    resp = requests.post(config.url + 'channel/addowner/v1', json={"token": user1_token, "channel_id": private_channel_id, "u_id": user2_id})
+    assert(resp.status_code == NO_ERROR)
+
+    resp = requests.post(config.url + 'channel/addowner/v1', json={"token": user2_token, "channel_id": private_channel_id, "u_id": user3_id})
+    assert(resp.status_code == NO_ERROR)
+
+    details_response = requests.get(config.url + 'channel/details/v2', params={"token": user1_token, "channel_id": private_channel_id})
+    assert(details_response.status_code == NO_ERROR)
+    details = json.loads(details_response.text)
+    
+    assert(details["is_public"] == False)
+    assert(details["name"] == "Minecraft")
+    members_list = details["all_members"] 
+    assert(len(members_list) == 3)
+    owner_members_list = details["owner_members"]
+    assert(len(owner_members_list) == 3)
+
+# test adds two owners to the public channel
+    user1_token = initial_data["user1_token"]
+    user2_id = initial_data["user2_id"]
+    user2_token = initial_data["user2_token"]
+    user3_id = initial_data["user3_id"]
+    public_channel_id = initial_data["public_channel_id"]
+
+    resp = requests.post(config.url + 'channel/addowner/v1', json={"token": user1_token, "channel_id": public_channel_id, "u_id": user2_id})
+    assert(resp.status_code == NO_ERROR)
+
+    resp = requests.post(config.url + 'channel/addowner/v1', json={"token": user2_token, "channel_id": public_channel_id, "u_id": user3_id})
+    assert(resp.status_code == NO_ERROR)
+
+    details_response = requests.get(config.url + 'channel/details/v2', params={"token": user1_token, "channel_id": public_channel_id})
+    assert(details_response.status_code == NO_ERROR)
+    details = json.loads(details_response.text)
+    
+    assert(details["is_public"] == True)
+    assert(details["name"] == "Rainbow Six Siege")
+    members_list = details["all_members"] 
+    assert(len(members_list) == 3)
+    owner_members_list = details["owner_members"]
+    assert(len(owner_members_list) == 3)
