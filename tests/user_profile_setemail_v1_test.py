7cef083aa436170f8bafda6de706247b2d836947
Your Name <z5375997@ad.unsw.edu.au>
2021-10-21 06:05:34 +1100

    Added tests for user/profile/setemail/v1

---
+++ tests/user_profile_setemail_v1_test.py
@@ -0,0 +1,61 @@
+import json
+from src.error import InputError
+import pytest
+import requests
+from src.config import url
+
+INPUT_ERROR = 400
+ACCESS_ERROR = 403
+NO_ERROR = 200
+
+URL = url + "user/profile/setemail/v1"
+
+@pytest.fixture
+def setup():
+    requests.delete(url + "clear/v1")
+
+    user1_info = {
+        "email" : "email1@gmail.com", 
+        "password" : "password1",
+        "name_first" : "John",
+        "name_last" : "Smith" 
+    }
+
+    response1 = requests.post(url + "auth/register/v2", json=user1_info)
+    tok1 = response1.json()["token"]
+    u_id1= response1.json()["auth_user_id"]
+
+    user2_info = {
+        "email" : "taken@email.com", 
+        "password" : "password1",
+        "name_first" : "John",
+        "name_last" : "Smith" 
+    }
+
+    response2 = requests.post(url + "auth/register/v2", json=user2_info)
+    tok2 = response2.json()["token"]
+    u_id2 = response2.json()["auth_user_id"]
+
+    return {
+        "tok1": tok1,
+        "u_id1": u_id1,
+        "tok2": tok2,
+        "u_id2": u_id2
+    }
+
+# test invalid token
+def test_invalid_token():
+    response = requests.put(URL, json={"token":1557, "email": "invalid_email.com"})
+    assert response.status_code == ACCESS_ERROR    
+
+# email does not match regex 
+def test_register_email_invalid(setup):
+    token = setup["tok"]
+    response = requests.put(URL, json={"token":token, "email": "invalid_email.com"})
+    assert response.status_code == INPUT_ERROR
+
+# email has already been registered
+def test_register_email_taken(setup):
+    token = setup["tok"]
+    response = requests.put(URL, json={"token":token, "email": "taken@email.com"})
+    assert response.status_code == INPUT_ERROR
