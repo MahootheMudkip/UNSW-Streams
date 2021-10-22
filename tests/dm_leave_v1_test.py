b1475ce054389801f3f0564b32137a01bc37e667
Jay <z5341020@ad.unsw.edu.au>
2021-10-22 15:00:55 +1100

    Tests for dm leave

---
+++ tests/dm_leave_v1_test.py
@@ -0,0 +1,141 @@
+from src import dm
+from tests.channel_details_v2_test import ACCESS_ERROR, INPUT_ERROR
+import pytest
+import requests
+import json
+from src.config import url
+
+INPUT_ERROR = 400
+ACCESS_ERROR = 403
+NO_ERROR = 200
+
+
+@pytest.fixture
+def data():
+    # clear used data
+    requests.delete(url + 'clear/v1')
+
+    # create user 1
+    response1 = requests.post(url + "auth/register/v2", json={
+        "email": "Big@mac.com",
+        "password": "32gfhbf",
+        "name_first": "big",
+        "name_last": "mac"
+
+    })
+    token1 = response1.json()["token"]
+
+    # create user 2
+    response2 = requests.post(url + "auth/register/v2", json={
+        "email": "elon@lavender.com",
+        "password": "dvfs8h9v4",
+        "name_first": "elon",
+        "name_last": "lavender"
+
+    })
+    token2 = response2.json()["token"]
+
+    # create user 3
+    response3 = requests.post(url + "auth/register/v2", json={
+        "email": "roses@red.com",
+        "password": "dvfs8h9v4",
+        "name_first": "violets",
+        "name_last": "blue"
+
+    })
+    token3 = response3.json()["token"]
+
+    #create dm 1
+    response1 = requests.post(url + "dm/create/v1", json={
+        "token": token3,
+        "u_ids": [0, 1]
+    })
+    dm_id1 = response1.json()["dm_id"]
+
+    #create dm 2
+    response2 = requests.post(url + "dm/create/v1", json={
+        "token": token1,
+        "u_ids": [1]
+    })
+    dm_id2 = response2.json()["dm_id"]
+
+    return {
+        "token1" : token1
+        "token2" : token2
+        "token3" : token3
+        "dm_id1" : dm_id1
+        "dm_id2" : dm_id2
+    }
+
+#check if token is invalid
+def test_dm_details_invalid_token(data):
+    response = requests.get(url + "dm/leave/v1", params={
+        "token" : "abcde",
+        "dm_id": data["dm_id"]
+    })
+    assert response.status_code == ACCESS_ERROR
+
+# dm_id given doesn't exist
+def test_dm_details_dm_id_invalid(data):
+    response = requests.get(url + "dm/leave/v1", params={
+        "token" : data["token2"],
+        "dm_id" : -5346
+    })
+    assert response.status_code == INPUT_ERROR
+
+# user not a part of the dm
+def test_dm_details_user_not_in_dm(data):
+    response = requests.get(url + "dm/leave/v1", params={
+        "token" : data["token3"],
+        "dm_id": data["dm_id2"]
+    })
+    assert response.status_code == ACCESS_ERROR
+
+#check if user has actually left
+def test_user_left(data):
+    
+    #when the owner leaves
+    response1 = requests.get(url + "dm/leave/v1", params={
+        "token" : data["token1"],
+        "dm_id": data["dm_id2"]
+    })
+    assert response1.status_code == NO_ERROR
+
+    #get a list of dms of user with token and test
+    dm_id_response1 = requests.get(url + "dm/list/v1", 
+    params = {
+        "token": token1
+    })
+    assert dm_id_response1.status_code == NO_ERROR
+    data1 = dm_id_response1.json()
+
+    #loop through the dms list and check that dm, user left isn't there
+    for dms in data1["dms"]:
+        for dm_ids in dms["dm_id"]:
+            #user id of token 1 is 0
+            assert (dm_ids != 0)
+
+
+    #when a normal user leaves
+    response2 = requests.get(url + "dm/leave/v1", params={
+        "token" : data["token0"],
+        "dm_id": data["dm_id1"]
+    })
+    assert response1.status_code == NO_ERROR
+
+    #get a list of dms of user with token and test
+    dm_id_response2 = requests.get(url + "dm/list/v1", 
+    params = {
+        "token": token1
+    })
+    assert dm_id_response2.status_code == NO_ERROR
+    data2 = dm_id_response1.json()
+    
+    #loop through the dms list and check that dm, user left isn't there
+    for dms in data2["dms"]:
+        for dm_ids in dms["dm_id"]:
+            #user id of token 1 is 0
+            assert (dm_ids != 0)
+
+
+
