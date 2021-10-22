58aa4bf3ae5c8ed6a08d21e08a7b92e7fddfe3ea
Jay <z5341020@ad.unsw.edu.au>
2021-10-22 16:07:37 +1100

    Bug fix for dm remove test

--- tests/dm_remove_v1_test.py
+++ tests/dm_remove_v1_test.py
@@ -60,24 +60,24 @@ def data():
     dm_id2 = response2.json()["dm_id"]
 
     return {
-        "token1" : token1
-        "token2" : token2
-        "token3" : token3
-        "dm_id1" : dm_id1
+        "token1" : token1,
+        "token2" : token2,
+        "token3" : token3,
+        "dm_id1" : dm_id1,
         "dm_id2" : dm_id2
     }
 
 #check if token is invalid
 def test_dm_details_invalid_token(data):
-    response = requests.get(url + "dm/remove/v1", params={
+    response = requests.delete(url + "dm/remove/v1", json={
         "token" : "abcde",
-        "dm_id": data["dm_id"]
+        "dm_id": data["dm_id1"]
     })
     assert response.status_code == ACCESS_ERROR
 
 # dm_id given doesn't exist
 def test_dm_details_dm_id_doesnt_exist_token(data):
-    response = requests.get(url + "dm/remove/v1", params={
+    response = requests.delete(url + "dm/remove/v1", json={
         "token" : data["token2"],
         "dm_id" : -434
     })
@@ -85,48 +85,47 @@ def test_dm_details_dm_id_doesnt_exist_token(data):
 
 # check if the person removing the dm is the owner
 def test_owner_check(data):
-    reponnse1   = requests.get(url + "dm/remove/v1", params={
-        "token" : data["token3"]
-        "dm_id" : 0
+    response1   = requests.delete(url + "dm/remove/v1", json={
+        "token" : data["token3"],
+        "dm_id" : data["dm_id1"]
     })
-    assert response1.status_code == AccessError("user not the owner")
+    assert response1.status_code == ACCESS_ERROR
 
-    reponnse2 = requests.get(url + "dm/remove/v1", params={
-        "token" : data["token1"]
-        "dm_id" : 0
+    response2 = requests.delete(url + "dm/remove/v1", json={
+        "token" : data["token1"],
+        "dm_id" : data["dm_id1"]
     })
     assert response2.status_code == NO_ERROR
 
 #check if the dm has been removed
 def test_check_dm_removed(data):
-    reponnse1 = requests.get(url + "dm/remove/v1", params={
-        "token" : data["token1"]
-        "dm_id" : 0
-    })
-
     
     #get a list of dms of user with token and test
-    response1 = requests.get(url + "dm/leave/v1", params={
-        "token" : data["token1"],
-        "dm_id": data["dm_id2"]
-    })
-    assert response1.status_code == NO_ERROR
-
-    
     dm_id_response1 = requests.get(url + "dm/list/v1", 
     params = {
-        "token": token1
+        "token": data["token1"]
     })
     assert dm_id_response1.status_code == NO_ERROR
     data1 = dm_id_response1.json()
+    assert (len(data1["dms"]) == 2)
 
-    #loop through the dms list and check that dm, user left isn't there
-    for dms in data1["dms"]:
-        for dm_ids in dms["dm_id"]:
-            #user id of token 1 is 0
-            assert (dm_ids != 0)
+    #remove a dm from dm's list 
+    response1 = requests.delete(url + "dm/remove/v1", json={
+        "token" : data["token1"],
+        "dm_id": data["dm_id1"]
+    })
+    assert response1.status_code == NO_ERROR
 
+    #git a list of dms of the same user and compare
+    dm_id_response2 = requests.get(url + "dm/list/v1", 
+    params = {
+        "token": data["token1"]
+    })
+    assert dm_id_response2.status_code == NO_ERROR
+    data2 = dm_id_response2.json()
 
+    assert (len(data2["dms"]) == 1)
+    
 
 
 
