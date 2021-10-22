4db4e2fd70b82b26876053d476ad84e1a058af6e
Jay <z5341020@ad.unsw.edu.au>
2021-10-22 17:15:28 +1100

    Implementation for send dm

--- src/dm.py
+++ src/dm.py
@@ -172,3 +172,59 @@ def dm_leave_v1(token, dm_id):
     data_store.set(store)
 
     return {}
+
+
+
+
+def message_senddm_v1(token, dm_id, message):
+
+
+
+    # get auth_user_id from token (this function handles all exceptions)
+    auth_user_id = get_auth_user_id(token)
+
+    store = data_store.get()
+    dms = store["dms"]
+
+    # specified dm doesn't exist
+    if dm_id not in dms.keys(): 
+        raise InputError("Invalid dm_id")
+
+    # if it reaches this point, the dm_id must be valid
+    dm_info = dms[dm_id]
+    dm_members = dm_info["members"]
+
+    if auth_user_id not in dm_members:
+        raise AccessError("Valid dm_id and authorised user not a member")
+
+    if not 1 <= len(message) <= 1000:
+        raise InputError("Invalid message length")
+
+    # at this point, everything is valid
+    dm_id_tracker = store["dm_id_tracker"]
+    dm_messages = dm_info["messages"]
+
+    # timestamp
+    dt = datetime.now()
+    timestamp = dt.replace(tzinfo=timezone.utc).timestamp()
+
+    new_message = {
+        "message_id":   message_id_tracker,
+        "u_id":         auth_user_id,
+        "message":      message,
+        "time_created": timestamp
+    }
+
+    # append new message_id to the channel messages list
+    dm_messages.append(message_id_tracker)
+
+    messages = store["messages"]
+    messages[message_id_tracker] = new_message
+
+    store["message_id_tracker"] = message_id_tracker + 1
+    data_store.set(store)
+
+    return {
+        "message_id": message_id_tracker
+    }
+
