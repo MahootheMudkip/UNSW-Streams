772549aca892e9ecd626fbe50ae238bba2ff3fa8
Maximilian Huh <z5363771@ad.unsw.edu.au>
2021-11-11 13:06:32 +1100

    changed

--- src/gen_timestamp.py
+++ src/gen_timestamp.py
@@ -1,20 +1,16 @@
 from datetime import datetime
 from pytz import timezone
 
-TIME_ZONE = 'Australia/Sydney'
-# this is the timezone we want
+TIME_ZONE = 'UTC'
+# time zone
 
-# generates a timestamp of the current time converted to the given timezone
+# generates a timestamp
 def generate_timestamp():
-    # current time in utc
-    utc_time = datetime.now(timezone('UTC'))
-    # convert to local time zone
-    local_time = utc_time.astimezone(timezone(TIME_ZONE))
-    # generate timestamp and cast to int
-    converted = int(local_time.timestamp())
-    return converted
+    # current time in utc timestamp
+    timestamp = int(datetime.now(timezone(TIME_ZONE)).timestamp())
+    return timestamp
 
-print(generate_timestamp())
-print(int(datetime.now().timestamp()))
+# print(generate_timestamp())
+# print(int(datetime.now().timestamp()))
     
 
